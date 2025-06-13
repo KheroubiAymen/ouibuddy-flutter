f3ba2d4e1fa5a68fd61b58b68b9b9677670158d4
KheroubiAymen <kheroubimedaymen@gmail.com>
2025-06-13 12:29:08 +0100

    Initial OuiBuddy Flutter AppD

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,14 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    
+    // SOLUTION D'URGENCE: Try-catch pour éviter le crash
+    do {
+      GeneratedPluginRegistrant.register(with: self)
+    } catch {
+      print("❌ [iOS] Erreur plugins: \(error) - Application continue sans certains plugins")
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
